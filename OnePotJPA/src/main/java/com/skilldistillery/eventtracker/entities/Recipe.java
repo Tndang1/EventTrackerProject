325a65192c406d6bb0e977019fb4d7d519c1e5c1
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-11-13 17:01:28 -0600

    pojo stubbed and tested

---
+++ OnePotJPA/src/main/java/com/skilldistillery/eventtracker/entities/Recipe.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.eventtracker.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Recipe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	//Getters/Setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//hashCode/equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recipe other = (Recipe) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
---
+++ OnePotJPA/src/test/java/com/skilldistillery/eventtracker/entities/RecipeTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.eventtracker.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RecipeTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Recipe recipe;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("opPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		recipe = em.find(Recipe.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Test
+	void test_Category_entity_mapping() {
+		assertNotNull(recipe);
+		assertEquals("Beef Stew", recipe.getName());
+	}
+
+}
+
