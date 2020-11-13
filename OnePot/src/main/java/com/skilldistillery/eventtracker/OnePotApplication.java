b374157635845b7976ebfff0d914688c9374c11b
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-11-13 17:41:49 -0600

    stub boot with jpa dependency

---
+++ OnePot/src/main/java/com/skilldistillery/eventtracker/OnePotApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePotApplication.class, args);
+	}
+
+}
