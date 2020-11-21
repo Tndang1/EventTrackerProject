4323e5613909aedc72f83785bd6b789100339752
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-11-20 18:36:49 -0600

    prep for front end

---
+++ OnePot/src/main/resources/static/js/eventTracker.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+	console.log('Script loaded');
+	init();
+});
+
+function init() {
+	console.log('Tester in init fucntion');
+};
+
+//xhr.open('PUT', 'api/pangolins/'+pangoId);
